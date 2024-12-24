1a48f855980d1f2e378502508fdeac34d9018bec
Mamba1990 <h.daoudim@outlook.com>
2024-12-24 14:53:49 +0100

    update reademe,create models,mobe db.js to server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,4 @@
-/*import dotenv from 'dotenv';
-import app from './app.js';
 
-dotenv.config();
-const PORT = process.env.PORT || 4000;
-
-// Start the server only if not in test mode
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
-}
-*/
 import dotenv from 'dotenv';
 import app from './app.js';
 import connectDB from './db.js';
@@ -18,11 +6,7 @@ import connectDB from './db.js';
 // Load environment variables
 dotenv.config(); 
 
-// Log all environment variables to check if MONGO_URI is loaded correctly
-//console.log('Loaded Environment Variables:', process.env);
 
-// Log the MongoDB URI to ensure it's loaded correctly
-//console.log(process.env.MONGO_URI);
 
 // Connect to the database
 connectDB();
